e23340f40bc1c83d90198cc79a002883cc7be499
Bruno Vittoria <brunovittoria1999@hotmail.com>
2024-03-10 19:47:59 +0100

    User Login Backend Ready

--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -1,12 +1,13 @@
 import { Router, Request, Response } from 'express'
-import { CreateUserController } from '../controllers/user/CreateUserController'
+import { CreateUserController }      from '../controllers/user/CreateUserController'
+import { AuthUserController }        from '../controllers/user/AuthUserController'
 
 const router = Router()
 
 
 // ---- ROTAS USER ---- //
-router.post('/users', new CreateUserController().handle) //a lógica para criar usuários está encapsulada no método handle do CreateUserController. Provavelmente, esse método irá extrair os dados da requisição (por exemplo, nome, e-mail, senha), criar um novo usuário e responder de volta ao cliente com os resultados da operação.
-
+router.post('/users',   new CreateUserController().handle) //a lógica para criar usuários está encapsulada no método handle do CreateUserController. Provavelmente, esse método irá extrair os dados da requisição (por exemplo, nome, e-mail, senha), criar um novo usuário e responder de volta ao cliente com os resultados da operação.
+router.post('/session', new AuthUserController().handle) //Com essa rota ira fazer o login
 
 
 
