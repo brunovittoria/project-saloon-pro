25dcd034640133135c696d977f8fe15be27334f0
Bruno Vittoria <brunovittoria1999@hotmail.com>
2024-03-11 20:54:44 +0100

    Controller, routes & Service create for detail users

--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -1,14 +1,15 @@
 import { Router, Request, Response } from 'express'
 import { CreateUserController }      from '../controllers/user/CreateUserController'
 import { AuthUserController }        from '../controllers/user/AuthUserController'
+import { DetailUserController } from '../controllers/user/UserDetailController'
 
 const router = Router()
 
 
 // ---- ROTAS USER ---- //
-router.post('/users',   new CreateUserController().handle) //a lógica para criar usuários está encapsulada no método handle do CreateUserController. Provavelmente, esse método irá extrair os dados da requisição (por exemplo, nome, e-mail, senha), criar um novo usuário e responder de volta ao cliente com os resultados da operação.
-router.post('/session', new AuthUserController().handle) //Com essa rota ira fazer o login
-
+router.post('/users',   new CreateUserController().handle)  //a lógica para criar usuários está encapsulada no método handle do CreateUserController. Provavelmente, esse método irá extrair os dados da requisição (por exemplo, nome, e-mail, senha), criar um novo usuário e responder de volta ao cliente com os resultados da operação.
+router.post('/session', new AuthUserController().handle)    //Com essa rota ira fazer o login
+router.get('/me',       new DetailUserController().handle)  //Com essa rota listamos DETALHES do USERS
 
 
 
