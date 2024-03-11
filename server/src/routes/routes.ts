cbea34f2ada87f829ba74a31c19cfcf783d6dd23
Bruno Vittoria <brunovittoria1999@hotmail.com>
2024-03-11 21:26:09 +0100

    Auth Middleware Created

--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -3,13 +3,15 @@ import { CreateUserController }      from '../controllers/user/CreateUserControl
 import { AuthUserController }        from '../controllers/user/AuthUserController'
 import { DetailUserController } from '../controllers/user/UserDetailController'
 
+import { isAuthenticated } from '../middlewares/isAuthenticated'
+
 const router = Router()
 
 
 // ---- ROTAS USER ---- //
 router.post('/users',   new CreateUserController().handle)  //a lógica para criar usuários está encapsulada no método handle do CreateUserController. Provavelmente, esse método irá extrair os dados da requisição (por exemplo, nome, e-mail, senha), criar um novo usuário e responder de volta ao cliente com os resultados da operação.
 router.post('/session', new AuthUserController().handle)    //Com essa rota ira fazer o login
-router.get('/me',       new DetailUserController().handle)  //Com essa rota listamos DETALHES do USERS
+router.get('/me', isAuthenticated, new DetailUserController().handle)  //Com essa rota listamos DETALHES do USERS
 
 
 
