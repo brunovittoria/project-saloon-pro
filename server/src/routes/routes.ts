d16097911332d4ed840624e403e4e79e83f67902
Bruno Vittoria <brunovittoria1999@hotmail.com>
2024-03-18 21:35:31 +0100

    Created Structure to check quantity of works

--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -10,21 +10,23 @@ import { isAuthenticated }              from '../middlewares/isAuthenticated'
 import { ListWorksController }          from '../controllers/works/ListWorksController'
 import { UpdateWorksController }        from '../controllers/works/UpdateWorksController'
 import { CheckSubsController }          from '../controllers/works/CheckSubsController'
+import { CountWorksController }         from '../controllers/works/CountWorksController'
 
 const router = Router()
 
 
 // ---- ROTAS USER ---- //
-router.post('/users',   new CreateUserController().handle)  //a lógica para criar usuários está encapsulada no método handle do CreateUserController. Provavelmente, esse método irá extrair os dados da requisição (por exemplo, nome, e-mail, senha), criar um novo usuário e responder de volta ao cliente com os resultados da operação.
-router.post('/session', new AuthUserController().handle)    //Com essa rota ira fazer o login
-router.get('/me', isAuthenticated, new DetailUserController().handle)  //Com essa rota listamos DETALHES do USERS
-router.put('/users', isAuthenticated, new UpdateUserController().handle) //Rota para editar nossas info de USERS
+router.post('/users',   new CreateUserController().handle)                          //a lógica para criar usuários está encapsulada no método handle do CreateUserController. Provavelmente, esse método irá extrair os dados da requisição (por exemplo, nome, e-mail, senha), criar um novo usuário e responder de volta ao cliente com os resultados da operação.
+router.post('/session', new AuthUserController().handle)                            //Com essa rota ira fazer o login
+router.get('/me', isAuthenticated, new DetailUserController().handle)               //Com essa rota listamos DETALHES do USERS
+router.put('/users', isAuthenticated, new UpdateUserController().handle)            //Rota para editar nossas info de USERS
 
 // ---- ROTAS SERVICOS ---- //
-router.post('/service', isAuthenticated, new CreateWorksController().handle) //Rota para criar serviços
-router.get('/services', isAuthenticated, new ListWorksController().handle) //Rota para Lista serviços
-router.put('/service', isAuthenticated, new UpdateWorksController().handle) //Rota para lidar com a modificaçao dos serviços
-router.get('/haircut/check', isAuthenticated, new CheckSubsController().handle) //Rota onde faz a checagem da assinatura
+router.post('/service', isAuthenticated, new CreateWorksController().handle)        //Rota para criar serviços
+router.get('/services', isAuthenticated, new ListWorksController().handle)          //Rota para Lista serviços
+router.put('/service', isAuthenticated, new UpdateWorksController().handle)         //Rota para lidar com a modificaçao dos serviços
+router.get('/service/check', isAuthenticated, new CheckSubsController().handle)     //Rota onde faz a checagem da assinatura
+router.get('/service/count', isAuthenticated, new CountWorksController().handle)    //Rota onde contamos qtd. de serviços
 
 
 export { router }
