bba6b41052f64fa4b81c49deb89573c3eeca4197
Bruno Vittoria <brunovittoria1999@hotmail.com>
2024-03-24 22:27:38 +0100

    Initial structure frontend

---
+++ client/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head"
+
+export default function Home(){
+  return(
+    <>
+      <Head>
+        <title>Saloon Pro - Il tuo sistema completo</title>   {/*Aqui usamos o titulo para melhorar nosso SEO*/}
+      </Head>
+      <div>
+        <h1>TESTE HOME</h1>
+      </div>
+    </>
+  )
+}
