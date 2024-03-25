68b7e7c49ca6006532390f23c2a6e58ab92a8a7c
Bruno Vittoria <brunovittoria1999@hotmail.com>
2024-03-25 20:21:45 +0100

    ChakraUI ready and working

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head"
+import { Flex, Text } from '@chakra-ui/react'
 
 export default function Home(){
   return(
@@ -6,9 +7,9 @@ export default function Home(){
       <Head>
         <title>Saloon Pro - Il tuo sistema completo</title>   {/*Aqui usamos o titulo para melhorar nosso SEO*/}
       </Head>
-      <div>
-        <h1>TESTE HOME</h1>
-      </div>
+      <Flex background="barber.900" height="100vh" alignItems="center" justifyContent="center"> {/*Aqui basicamente importamos uma DIV que ja vem com o display flex*/}
+        <Text fontSize={30}>Pagina HOME</Text>
+      </Flex>
     </>
   )
 }
