6d8d407c8addb662e28ed2140a58977a81aed5b2
Bruno Vittoria <brunovittoria1999@hotmail.com>
2024-04-22 21:04:36 +0200

    Listing Service

--- client/src/pages/servicos/index.tsx
+++ client/src/pages/servicos/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
 import { Sidebar } from '../../components/sidebar'
@@ -12,10 +13,27 @@ import {
 } from '@chakra-ui/react'
 
 import { IoMdPricetag } from 'react-icons/io'
+import { canSSRAuth } from '../../utils/canSSRAuth'
+import { setupAPIClient } from '../../services/api'
 
-export default function Servicos(){
+
+interface ServicesItem{
+    id: string
+    name: string
+    price: number | string
+    status: boolean
+    user_id: string
+}
+
+interface ServicesProps{            //Dessa forma carregamos todas as informaçoes necessarios sobre os serviços dentro de um unico ARRAY
+    services: ServicesItem[]
+}
+
+export default function Servicos({ services }: ServicesProps ){
 
     const [isMobile] = useMediaQuery("(max-width: 500px)")
+    const [serviceList, setServiceList] = useState<ServicesItem[]>(services || [])        //Como estado inicial de nossa STATE passamos a lista de servicos com um GENERIC do TS
+
     return(
         <>
             <Head>
@@ -29,7 +47,7 @@ export default function Servicos(){
                             Miei Servizi
                         </Heading>
 
-                        <Link href="/services/new">
+                        <Link href="/servicos/new">
                             <Button>
                                 Crea Nuovo
                             </Button>
@@ -45,24 +63,26 @@ export default function Servicos(){
 
                     </Flex>
 
-                    <Link href="/haircuts/123" > {/*Essa propriedade serve pra quando queremos colocar um flex dentro do legacyBehavior */}
+                    {serviceList.map(services => (
+                        <Link key={services.id} href={ `/servicos/${services.id}`} > {/*Essa propriedade serve pra quando queremos colocar um flex dentro do legacyBehavior */}
                             
-                            <Flex cursor="pointer" w="100%" p={4} bg="barber.400" align={isMobile ? "flex-start" : "center"} direction={isMobile ? "column" : "row"} rounded="4" mb={2} justifyContent="space-between">
-                                
-                                <Flex mb={isMobile ? 2 : 0} direction="row" alignItems="center" justifyContent="center">
-                                    <IoMdPricetag size={28} color="#fba931"/>
-                                        <Text fontWeight="bold" ml={4} color="white">
-                                            Taglio Uomo
-                                        </Text>
-                                </Flex>
-
-                                <Text>
-                                Prezzo: €20.00
-                                </Text>
-
+                        <Flex cursor="pointer" w="100%" p={4} bg="barber.400" align={isMobile ? "flex-start" : "center"} direction={isMobile ? "column" : "row"} rounded="4" mb={2} justifyContent="space-between">
+                            
+                            <Flex mb={isMobile ? 2 : 0} direction="row" alignItems="center" justifyContent="center">
+                                <IoMdPricetag size={28} color="#fba931"/>
+                                    <Text fontWeight="bold" ml={4} color="white">
+                                        {services.name}
+                                    </Text>
                             </Flex>
-                            
-                    </Link>
+
+                            <Text>
+                            Prezzo: € {services.price}
+                            </Text>
+
+                        </Flex>
+                        
+                </Link>
+                    ))}
 
                 </Flex>
 
@@ -70,3 +90,38 @@ export default function Servicos(){
         </>
     )
 }
+
+export const getServerSideProps = canSSRAuth(async (ctx) => {              
+
+    try{
+
+        const apiClient = setupAPIClient(ctx)
+        const response = await apiClient.get("/services", { //Assim que renderizar a pagina iremos renderizar todos os servicos
+            params:{
+                status: true,
+            }
+        })                
+
+        if(response.data === null){
+            return{
+                redirect:{
+                    destination: '/dashboard',
+                    permanent: false,
+                }
+            }
+        }
+
+        console.log("response lista:", response.data)
+
+        
+        return{
+            props: {
+                services: response.data
+            }
+        }
+    }catch(err){
+        console.log(err)
+        
+    }
+
+})
