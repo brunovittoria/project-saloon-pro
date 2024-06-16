ac16a1a37be8c63ad06dab76047f9ce2dce42cad
Bruno Vittoria <brunovittoria1999@hotmail.com>
2024-06-16 17:39:26 +0200

    FEAT: Changed btn styles and Heading

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head"
-import { Text, Button } from '@chakra-ui/react'
+import { Button } from '@/components/ui/button'
+import Typography from '@/components/ui/typography'
 import Image from 'next/image'
 import Link from "next/link"
 import { ArrowUpDown, Timer, Workflow } from 'lucide-react'
@@ -17,19 +18,19 @@ export default function Home(){
         w-full items-center text-center gap-12"
     >
       <div className="flex flex-col gap-6 items-center">
-        <Text className="max-w-2xl" variant="h1">
+        <Typography className="max-w-2xl" variant="h1">
           Information you need during on-call emergencies
-        </Text>
-        <Text className="max-w-2xl" variant="h5">
+        </Typography>
+        <Typography className="max-w-2xl" variant="h5">
           Quickly link new on-call tickets to similar past
           incidents and their solutions. All directly in
           Slack the moment an incident happens.
-        </Text>
+        </Typography>
         <Link
           href="https://map.sistilli.dev/public/coding/SaaS+Boilerplate"
           target="_blank"
         >
-          <Button size="tiny" variant="ghost">
+          <Button size="tiny" variant="ghost" color="#white">
             {`Get Started`}
           </Button>
         </Link>
@@ -42,9 +43,9 @@ export default function Home(){
       </div>
       <div className="flex flex-col md:pt-24 md:gap-36 gap-24 items-center">
         <div className="flex flex-col gap-12 items-center">
-          <Text className="max-w-2xl" variant="h1">
+          <Typography className="max-w-2xl" variant="h1">
             Quick solutions, less stress
-          </Text>
+          </Typography>
           <div className="flex md:flex-row flex-col gap-12">
             <Feature
               icon={<Timer size={24} />}
@@ -64,14 +65,14 @@ export default function Home(){
           </div>
         </div>
         <div className="flex flex-col gap-6 max-w-2xl items-center">
-          <Text className="max-w-2xl" variant="h1">
+          <Typography className="max-w-2xl" variant="h1">
             Instant setup, no custom code
-          </Text>
-          <Text className="max-w-2xl" variant="p">
+          </Typography>
+          <Typography className="max-w-2xl" variant="p">
             Quickly link new on-call tickets to similar past
             incidents and their solutions. All directly in
             Slack the moment an incident happens.
-          </Text>
+          </Typography>
           <Image
             width={1024}
             height={632}
@@ -80,9 +81,9 @@ export default function Home(){
           />
         </div>
         <div className="flex flex-col gap-6 items-center">
-          <Text className="max-w-2xl" variant="h1">
+          <Typography className="max-w-2xl" variant="h1">
             Get in touch
-          </Text>
+          </Typography>
           <div>Book a demo, or hop on a call</div>
           <Link
             href="https://map.sistilli.dev/public/coding/SaaS+Boilerplate"
